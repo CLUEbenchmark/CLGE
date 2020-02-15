26b4cd1cd9faf2b3b4b5918db0c17e6d45982032
AkiraKanade <54142993+AkiraKanade@users.noreply.github.com>
2020-02-15 18:04:57 +0800

    Update run_lstm.sh

--- tasks/csl/run_lstm.sh
+++ tasks/csl/run_lstm.sh
@@ -10,15 +10,6 @@ export GLUE_DATA_DIR=$CURRENT_DIR/../../CLGEdataset
 
 # check python package 
 
-check_keras=`pip show keras | grep "Version"`
-
-if [ ! -n "$check_keras" ]; then
-  pip install keras==2.2.4
-else
-  pip install keras==2.2.4
-fi
-
-
 check_keras_layer_normalization=`pip show keras_layer_normalization | grep "Version"`
 
 if [ ! -n "$check_keras_layer_normalization" ]; then
