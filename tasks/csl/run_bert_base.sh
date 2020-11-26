f69923121874c2ee6742908ac1271d9fdb731454
diandian737 <58292649+diandian737@users.noreply.github.com>
2020-11-26 10:54:39 +0800

    Update run_bert_base.sh

--- tasks/csl/run_bert_base.sh
+++ tasks/csl/run_bert_base.sh
@@ -14,12 +14,12 @@ export GLUE_DATA_DIR=$CURRENT_DIR/../../CLGEdataset
 check_bert4keras=`pip show bert4keras | grep "Version"`
 
 if [ ! -n "$check_bert4keras" ]; then
-  pip install git+https://www.github.com/bojone/bert4keras.git@v0.3.6
+  pip install git+https://www.github.com/bojone/bert4keras.git@v0.8.3
 else
-  if [  ${check_bert4keras:8:13} = '0.3.6' ] ; then
+  if [  ${check_bert4keras:8:13} = '0.8.3' ] ; then
     echo "bert4keras installed."
   else
-    pip install git+https://www.github.com/bojone/bert4keras.git@v0.3.6
+    pip install git+https://www.github.com/bojone/bert4keras.git@v0.8.3
   fi
 fi
 
